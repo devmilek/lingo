4b7312017daa1bea22cd18c193a98fb1db388348
devmilek <devmilek@gmail.com>
2024-03-19 13:13:01 +0100

    style buttons

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <main>hello twink</main>
+    <div className="p-4 space-y-4">
+      <Button>Primary</Button>
+      <Button>Primary outline</Button>
+    </div>
   );
 }
